d942dfc6c35a028b1b05547b0b4ede4a2bad6471
sydmasina <masinasydwell@gmail.com>
2022-06-28 19:44:12 +0200

    Import route module to main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router.js';
 
-createApp({}).mount('#app');
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
