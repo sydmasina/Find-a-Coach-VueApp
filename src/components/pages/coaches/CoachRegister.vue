86855de29ea18a3915ce8d0de8ab663ab5be4ed9
sydmasina <masinasydwell@gmail.com>
2022-06-30 16:09:33 +0200

    Add Coach Form component to coach registration page

--- src/components/pages/coaches/CoachRegister.vue
+++ src/components/pages/coaches/CoachRegister.vue
@@ -1,15 +1,22 @@
 <template>
   <section>
+    <base-card>
     <h2>Register as a Coach now!</h2>
+    <coach-form></coach-form>
+    </base-card>
     
   </section>
 </template>
 
 <script>
+import CoachForm from '../../minicomps/coaches/CoachForm.vue';
 export default {
-
+  components: {
+    CoachForm,
+  }
 }
 </script>
+    BaseCard
 
 <style>
 
