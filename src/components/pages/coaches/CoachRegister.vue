0438feda594858f352e19b63607fead3127a7c00
sydmasina <masinasydwell@gmail.com>
2022-06-30 17:37:25 +0200

    Complete Coach Registration Form functionality

--- src/components/pages/coaches/CoachRegister.vue
+++ src/components/pages/coaches/CoachRegister.vue
@@ -1,10 +1,9 @@
 <template>
   <section>
     <base-card>
-    <h2>Register as a Coach now!</h2>
-    <coach-form></coach-form>
+      <h2>Register as a Coach now!</h2>
+      <coach-form @save-data="saveData"></coach-form>
     </base-card>
-    
   </section>
 </template>
 
@@ -13,11 +12,16 @@ import CoachForm from '../../minicomps/coaches/CoachForm.vue';
 export default {
   components: {
     CoachForm,
+  },
+  methods: {
+    saveData(data){
+      this.$store.dispatch('coaches/registerCoach', data);
+      this.$router.push('/coaches');
+    }
   }
-}
+};
 </script>
     BaseCard
 
 <style>
-
 </style>
