100dda18c82b9bff16501f3991c432f1894d6bec
sydmasina <masinasydwell@gmail.com>
2022-06-30 15:24:03 +0200

    Build Coach Detail Component... Create coach filter component

--- src/components/pages/coaches/CoachDetails.vue
+++ src/components/pages/coaches/CoachDetails.vue
@@ -1,15 +1,67 @@
 <template>
-    <h1>Coach Details</h1> 
-    <router-view></router-view>
-    <router-link to="/coaches/c1/contact">Contact</router-link>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>${{ rate }}/hour</h3>
+    </base-card>
+  </section>
+  <section>
+    <header>
+      <base-card>
+        <header>
+          <h2>Interested? Reach out now!</h2>
+          <base-button link :to="contactLink">Contact</base-button>
+        </header>
+        <router-view></router-view>
+      </base-card>
+    </header>
+    <section>
+      <base-card>
+        <base-badge
+          v-for="area in areas"
+          :key="area"
+          :title="area"
+          :type="area"
+        ></base-badge>
+        <p>{{ description }}</p>
+      </base-card>
+    </section>
+  </section>
 </template>
 
 <script>
 export default {
-
-}
+  props: ['id'],
+  data() {
+    return {
+      selectedCoach: null,
+    };
+  },
+  computed: {
+    fullName() {
+      return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName;
+    },
+    contactLink() {
+      return this.$route.path + '/contact';
+    },
+    areas() {
+      return this.selectedCoach.areas;
+    },
+    description() {
+      return this.selectedCoach.description;
+    },
+    rate() {
+      return this.selectedCoach.hourlyRate;
+    },
+  },
+  created() {
+    this.selectedCoach = this.$store.getters['coaches/coaches'].find(
+      (coach) => coach.id === this.id
+    );
+  },
+};
 </script>
 
 <style>
-
 </style>
+BaseCard
--- src/router.js
+++ src/router.js
@@ -12,15 +12,17 @@ import NotFound from './components/pages/NotFound.vue';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path: '/', redirect: '/coaches'},
-        {path: '/coaches', component: CoachList},
-        {path: '/coaches/:id', component: CoachDetails, 
-        children: [
-            { path: 'contact', component: ContactCoach},
-        ]},
-        {path: '/register', component: CoachRegister},
-        {path: '/requests', component: RequestList},
-        {path: '/:notFound(.*)', component: NotFound},
+        { path: '/', redirect: '/coaches' },
+        { path: '/coaches', component: CoachList },
+        {
+            path: '/coaches/:id', component: CoachDetails, props: true,
+            children: [
+                { path: 'contact', component: ContactCoach },
+            ]
+        },
+        { path: '/register', component: CoachRegister },
+        { path: '/requests', component: RequestList },
+        { path: '/:notFound(.*)', component: NotFound },
     ]
 });
 
