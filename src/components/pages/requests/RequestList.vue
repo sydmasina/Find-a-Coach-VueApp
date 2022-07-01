6e4604b07639e1d1e9ab42d1365f059cebbae0b2
sydmasina <masinasydwell@gmail.com>
2022-07-01 17:29:27 +0200

    Create Request Item and ouput incoming requests

--- src/components/pages/requests/RequestList.vue
+++ src/components/pages/requests/RequestList.vue
@@ -1,13 +1,53 @@
 <template>
-  <h1>Requests Lists</h1>
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests Recieved</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <request-item
+          v-for="request in recievedRequests"
+          :key="request.id"
+          :email="request.userEmail"
+          :message="request.message"
+        ></request-item>
+      </ul>
+      <h3 v-else>No requests Recieved Yet</h3>
+    </base-card>
+  </section>
 </template>
 
 <script>
-export default {
+import RequestItem from '../../minicomps/requests/RequestItem.vue';
 
-}
+export default {
+  components: {
+    RequestItem,
+  },
+  computed: {
+    recievedRequests() {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+header {
+  text-align: center;
+}
 
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
 </style>
--- src/components/store/modules/requests/getters.js
+++ src/components/store/modules/requests/getters.js
@@ -1,3 +1,8 @@
 export default {
-    
+    requests(state){
+        return state.requests;
+    },
+    hasRequests(state){
+        return state.requests && state.requests.length > 0;  //Returns true if there's at least one request.
+    }
 }
