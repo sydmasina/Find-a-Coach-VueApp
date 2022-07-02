dd6e408c51eebfbea7a47b89d4d4b593bfd82dfc
sydmasina <masinasydwell@gmail.com>
2022-07-02 14:19:44 +0200

    Update 'page not found' page

--- src/components/pages/NotFound.vue
+++ src/components/pages/NotFound.vue
@@ -1,13 +1,15 @@
 <template>
-  <h1>Page Not found!!!!</h1>
+  <section>
+    <base-card>
+      <h2>Page Not found!!</h2>
+      <p>Please double check your link and try again.</p>
+    </base-card>
+  </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
