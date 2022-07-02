fdc63d0440bf865d5d4e6bcdea1a04fbf993fa3b
sydmasina <masinasydwell@gmail.com>
2022-07-02 18:53:58 +0200

    Add Signup functionality

--- src/components/pages/auth/UserAuth.vue
+++ src/components/pages/auth/UserAuth.vue
@@ -54,6 +54,15 @@ export default {
       ) {
         this.formIsValid = false;
       }
+
+      if(this.mode === 'login'){
+        //..
+      }else{
+        this.$store.dispatch('signup', {
+            email: this.email,
+            password: this.password,
+        });
+      }
     },
     switchAuthMode() {
       if (this.mode === 'login') {
