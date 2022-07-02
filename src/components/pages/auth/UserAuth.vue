42d39d2f0137153922c3778a4537ca66ede0cccb
sydmasina <masinasydwell@gmail.com>
2022-07-02 16:56:55 +0200

    Add more functionality to UserAuth Component

--- src/components/pages/auth/UserAuth.vue
+++ src/components/pages/auth/UserAuth.vue
@@ -1,25 +1,74 @@
 <template>
-  <form>
-    <base-card>
+  <base-card>
+    <form @submit.prevent="submitForm">
       <div class="form-control">
         <label for="email">Email</label>
-        <input type="email" id="email" />
+        <input type="email" id="email" v-model.trim="email" />
       </div>
       <div class="form-control">
         <label for="password">Password</label>
-        <input type="password" id="password" />
+        <input type="password" id="password" v-model.trim="password" />
       </div>
-      <base-button>Login</base-button>
-      <base-button type="button" mode="flat">Signup instead</base-button>
-    </base-card>
-  </form>
+      <p v-if="!formIsValid" class="errors">
+        Incorrect email or password. Check details and try again.
+      </p>
+      <base-button>{{ submitButtonCaption }}</base-button>
+      <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchModeButtonCaption}}</base-button>
+    </form>
+  </base-card>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      formIsValid: true,
+      mode: 'login',
+    };
+  },
+  computed: {
+    submitButtonCaption(){
+        if(this.mode === 'login'){
+            return 'Login';
+        }else{
+            return 'Signup';
+        }
+    },
+    switchModeButtonCaption(){
+        if(this.mode === 'login'){
+            return 'Signup instead';
+        }else{
+            return 'Login instead';
+        }
+    },
+  },
+  methods: {
+    submitForm() {
+      this.formIsValid = true;
+      if (
+        this.email === '' ||
+        !this.email.includes('@') ||
+        this.password.length < 6
+      ) {
+        this.formIsValid = false;
+      }
+    },
+    switchAuthMode() {
+      if (this.mode === 'login') {
+        this.mode = 'signup';
+      } else {
+        this.mode = 'login';
+      }
+    },
+  },
+};
+</script>
 
 <style scoped>
 form {
   margin: 1rem;
-  border: 1px solid #ccc;
-  border-radius: 12px;
   padding: 1rem;
 }
 
@@ -47,5 +96,10 @@ textarea:focus {
   border-color: #3d008d;
   background-color: #faf6ff;
   outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
 }
 </style>
